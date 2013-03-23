b601bc1832f1096cdc7aed456010ca829795c344
Michael Richardson <contact@thoughttransmitter.com>
2013-03-23 20:55:42 +0000

    Created Main SUDS Anxiety Level Input Activity

--- src/com/inputprocessoutput/pockettherappist/PocketTherAPPist.java
+++ src/com/inputprocessoutput/pockettherappist/PocketTherAPPist.java
@@ -3,6 +3,9 @@ package com.inputprocessoutput.pockettherappist;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class PocketTherAPPist extends Activity {
 
@@ -10,6 +13,32 @@ public class PocketTherAPPist extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pocket_ther_appist);
+        
+        SeekBar sb1=(SeekBar) findViewById(R.id.suds_slider);
+        sb1.setMax(100);
+        sb1.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                    boolean fromUser) {
+            	TextView tv1 = (TextView) findViewById(R.id.suds_level_display);
+            	tv1.setText(Integer.toString(progress));
+
+            }
+        });
+        
     }
 
 
